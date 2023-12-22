ed5dc8efa4dcd17914759afed3a934e9b655822e
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2023-12-22 22:28:52 +0545

    Update SD_02.py

--- SD_02.py
+++ SD_02.py
@@ -4,3 +4,6 @@ def guessing_game():
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100. Can you guess it?")
     
+    # Generate a random number between 1 and 100
+    secret_number = random.randint(1, 100)
+    
